c4ee33c6e6a7aab2fa183ab2368b53e64ce20abd
Javier Franco <notfrannco@gmail.com>
2025-02-19 14:57:15 -0300

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,19 @@
 require 'core.options'
 require 'core.keymaps'
 
+-- remove padding 
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function() io.write("\027]111\027\\") end,
+})
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
